9c082ab64d7ffdd5499667239b96aab48520ac58
danyberna2002@gmail.com <danyberna2002@gmail.com>
2026-01-21 12:32:03 -0500

    Ejemplo01

--- EjemplosExcepciones/src/paquete02/Ejemplo01.java
+++ EjemplosExcepciones/src/paquete02/Ejemplo01.java
@@ -26,9 +26,9 @@ public class Ejemplo01 {
             try {
                 Scanner entrada = new Scanner(System.in);
                 System.out.println("Ingrese valor 1:");
-                int valor1 = entrada.nextInt(); // se espera un valor entero 
+                int valor1 = entrada.nextInt();
                 System.out.println("Ingrese valor 2:");
-                int valor2 = entrada.nextInt(); // se espera un valor entero // 0
+                int valor2 = entrada.nextInt(); 
                 int resultado = valor1 / valor2;
 
                 System.out.printf("Resultado %d\n", resultado);
